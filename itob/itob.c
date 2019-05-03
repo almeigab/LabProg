364990aa389b325a7e0d11da13efe277f01f7abb
Gabriel <gabrielalmeida140395@gmail.com>
2019-05-02 21:56:51 -0300

    final version itob

--- itob/itob.c
+++ itob/itob.c
@@ -5,9 +5,9 @@ int itob(int n, char s[], int b);
 
 int main ()
 {
-    int n = 250;
+    int n = 53810;
     char s[100];
-    int b = 23; 
+    int b = 36; 
     int res = itob(n,s,b);
     if (!res)
 		printf("%s\n",s);
@@ -24,36 +24,28 @@ int itob (int n, char s[], int b)
     //calcula o valor absoluto do número  
     n = sign * n;
 
-    //inicializa o primeiro resto como o próprio número
-    int resto = n;
-
     //retorna erro para bases inválidas
     if (b < 2 || b > 36)
 		return -1;
 
-    //calcula os restos das divisões dos restos anteriores pela base
+    //calcula os restos iterativamente
+    int resto;
     int i = 0;
-    while(n >= b) {
+    while(n > 0) {
         resto = n%b;
-        n = n/b;
+        n /= b;
         if(resto >= 0 && resto <= 9)
             s[i++] = resto + '0';
-        else if (resto > 9)
+        else
             s[i++] = resto - 10 + 'A';
     }
-
-    //calcula o último quociente
-    if(n >= 0 && n <= 9)
-        s[i] = n + '0';
-    else if (n > 9)
-        s[i] = n - 10 + 'A';
     
     //adiciona o sinal
     if(sign == -1)
-        s[++i]='-';
+        s[i++]='-';
     
     //finaliza a string
-    s[++i]='\0';
+    s[i]='\0';
 
     //inverte a string
     int length = i;
