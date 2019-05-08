433a7ea427c455480015b7b8971796b26c91b2c4
Gabriel Almeida de Oliveira <gabrielalmeida140395@gmail.com>
2019-05-08 14:10:43 -0300

    finished criptografia

--- criptografia/criptografia.c
+++ criptografia/criptografia.c
@@ -5,43 +5,40 @@ void maiuscula (char s[]);
 void primeiroChar (char s[], char key[]);
 void completaChave (char s[]);
 void criptografaFrase (char s[], char key[]);
-void leFrase (char s[], int size);
-// int getInputSize(char s[]);
-// void concatInput(char *s[], int n, char res[]);
+void validaFrase (char s[]);
 
 int main () {
-// int main (int argc, char **argv){
-    // int i, size = argc - 1;
-    // for (i = 1; i < argc; i++ ) {
-    //     size += getInputSize(argv[i]);
-    // }
-    // char string[size];
-    // concatInput(argv, argc, string);
-    char string1[] = "Celacan123to provoca mar123emoto1";
+    char string1[1000];
     char key[30];
     char string2[1000];
-    printf("\nstring1:\n%s\n",string1);
-    converteFraseEmChave(string1,key);//conversão de frase em chave
-    printf("\nkey:\n%s\n",key);
-    printf("\nInsira a frase:\n");
-    leFrase(string2,1000); //lê frase pelo teclado a ser criptografada
-    printf("\nstring2:\n%s\n",string2);
+    
+    printf("\nInsira a frase a se tornar chave:\n");
+    fgets(string1,1000,stdin);
+    
+    printf("\nInsira a frase a ser criptografada:\n");
+    fgets(string2,1000,stdin );;
+    
+    converteFraseEmChave(string1,key);//converte frase em chave
+    printf("\nchave:\n%s\n",key);
+    
+    validaFrase(string2); //valida frase a ser criptografada
     criptografaFrase(string2,key); //criptografa frase
-    printf("\nstring2 convertida:\n%s\n",string2);
+    printf("\nfrase convertida:\n%s\n",string2);
+    
     return 0;
 }
 
 /******************************
-    conversão de frase em chave
+    converte de frase em chave
 *******************************/
 void converteFraseEmChave (char s[], char key[]){
-    maiuscula(s); //conversão das letras minúsculas em maiúsculas
-    primeiroChar(s,key); //eliminação de caracter inválido e verificação de primeira ocorrência
+    maiuscula(s); //converte letras minúsculas em maiúsculas
+    primeiroChar(s,key); //elimina caracter inválido e verificação de primeira ocorrência
     completaChave(key); //completa chave com caracteres restantes
 }
 
 /******************************
-    conversão das letras
+    converte das letras
     minúsculas em maiúsculas
 *******************************/
 void maiuscula (char s[]) {
@@ -52,7 +49,7 @@ void maiuscula (char s[]) {
 }
 
 /******************************
-    eliminação de caracter
+    elimina de caracter
     inválido e verificação de
     primeira ocorrência
 *******************************/
@@ -106,7 +103,7 @@ void completaChave (char s[]) {
     criptografa frase
 *******************************/
 void criptografaFrase (char s[], char key[]) {
-    int index = 0, i = 0, j = 0;
+    int index = 0, i = 0;
     //criptografa
     for (i=0; s[i]!='\0'; i++) {
         //indice é movido para direita pelo valor do char
@@ -126,37 +123,19 @@ void criptografaFrase (char s[], char key[]) {
 }
 
 /******************************
-    lê frase pelo teclado
+    valida frase
     a ser criptografada
 *******************************/
-void leFrase (char s[], int size) {
-    int i = 0;
-    int c;
-	while (i < size && (c = getchar())!= EOF && (c = getchar())!='\n' ) {
-        //converte e maiuscula
-        if(c >= 'a' && c <='z')
-            c += 'A' - 'a';
-        //verifica se char é válido
-        if ( (c >= 'A' && c <= 'Z') || c == ',' || c == '.' || c == ' ')
-            s[i++] = c;
-    }
-    s[i] = '\0';
+void validaFrase (char s[]) {
+    int i = 0, j;
+    maiuscula(s); //converte das letras minúsculas em maiúsculas
+    // loop que ignora char inválido
+    while (s[i] != '\0') {
+		while ( s[i] != '\0' && !((s[i] >= 'A' && s[i] <= 'Z') || s[i] == ',' || s[i] == '.' || s[i] == ' ') ) {
+			for (j = i; s[j] != '\0'; j++)
+				s[j] = s[j+1];
+			s[++j] = '\0';
+		}
+        i++;
+	}
 }
-
-// int getInputSize(char s[]) {
-//     int i, size = 0;
-//     for (i = 0; s[i] != '\0'; i++)
-//         size++;
-//     return size;
-// }
-
-// void concatInput(char *s[], int n, char res[]) {
-//     int i, j, k = 0;
-//     for(i = 1; i < n; i++) {
-//         for (j = 0; s[i][j] != '\0'; j++) {
-//             res[k++] = s[i][j];
-//         }
-//         res[k++] = ' ';
-//     }
-//     res[k] = '\0';
-// }
